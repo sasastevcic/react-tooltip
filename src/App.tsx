fa39102725276d4e00b3c9e3ecb52079dc6f0778
Sasa Stevcic <sasa.stevcic@yahoo.com>
2022-06-28 15:43:51 +0200

    Use tabs

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,31 @@ import Tooltip from './Tooltip';
 import { TooltipType } from './Tooltip/Tooltip.data';
 
 function App() {
-  return (
-    <div className="App" style={{ backgroundImage: 'url("/background.jpg")' }}>
-      <Tooltip text="Small text">
-        <StyledButton $buttonTheme={TooltipType.Default}>Default</StyledButton>
-      </Tooltip>
-      <div style={{ height: 20 }}></div>
-      <Tooltip
-        type={TooltipType.Success}
-        placement="left-end"
-        text="Some loong content Some loong content Some loong content Some
+	return (
+		<div className="App" style={{ backgroundImage: 'url("/background.jpg")' }}>
+			<Tooltip text="Small text">
+				<StyledButton $buttonTheme={TooltipType.Default}>Default</StyledButton>
+			</Tooltip>
+			<div style={{ height: 20 }}></div>
+			<Tooltip
+				type={TooltipType.Success}
+				placement="left-end"
+				text="Some loong content Some loong content Some loong content Some
 								loong content Some loong content Some loong content Some loong
 								content Some loong content Some loong content Some loong content"
-      >
-        <StyledButton $buttonTheme={TooltipType.Success}>Success</StyledButton>
-      </Tooltip>
-      <div style={{ height: 20 }}></div>
-      <Tooltip
-        type={TooltipType.Error}
-        placement="right"
-        text="Some error message"
-      >
-        <StyledButton $buttonTheme={TooltipType.Error}>Error</StyledButton>
-      </Tooltip>
-    </div>
-  );
+			>
+				<StyledButton $buttonTheme={TooltipType.Success}>Success</StyledButton>
+			</Tooltip>
+			<div style={{ height: 20 }}></div>
+			<Tooltip
+				type={TooltipType.Error}
+				placement="right"
+				text="Some error message"
+			>
+				<StyledButton $buttonTheme={TooltipType.Error}>Error</StyledButton>
+			</Tooltip>
+		</div>
+	);
 }
 
 export default App;
--- src/Tooltip/Tooltip.data.ts
+++ src/Tooltip/Tooltip.data.ts
@@ -2,32 +2,32 @@ import { Placement } from '@floating-ui/react-dom';
 import { CSSProperties, ReactNode } from 'react';
 
 export enum TooltipType {
-  Default = 'default',
-  Success = 'success',
-  Error = 'error',
+	Default = 'default',
+	Success = 'success',
+	Error = 'error',
 }
 
 export interface TooltipProps {
-  text: string;
-  children: ReactNode;
-  placement?: Placement;
-  type?: TooltipType;
-  offset?: number;
+	text: string;
+	children: ReactNode;
+	placement?: Placement;
+	type?: TooltipType;
+	offset?: number;
 }
 
 export type ArrowCSSProperties = Pick<
-  CSSProperties,
-  'top' | 'bottom' | 'left' | 'right' | 'transform'
+	CSSProperties,
+	'top' | 'bottom' | 'left' | 'right' | 'transform'
 >;
 
 export type ColorProps = {
-  $backgroundColor: string;
+	$backgroundColor: string;
 };
 
 export const mapColors: Record<TooltipType, string> = {
-  [TooltipType.Default]: 'black',
-  [TooltipType.Success]: 'green',
-  [TooltipType.Error]: 'red',
+	[TooltipType.Default]: 'black',
+	[TooltipType.Success]: 'green',
+	[TooltipType.Error]: 'red',
 };
 
 export const ARROW_SIZE = 16;
