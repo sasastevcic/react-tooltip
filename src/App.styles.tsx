11d4fb6b2288624b5d33fc82a4310d9e3479f080
Sasa Stevcic <sasa.stevcic@yahoo.com>
2022-06-28 15:38:26 +0200

    Use spaces

--- src/App.styles.tsx
+++ src/App.styles.tsx
@@ -1,44 +1,44 @@
 import styled, {
-	css,
-	DefaultTheme,
-	FlattenInterpolation,
-	ThemeProps,
+  css,
+  DefaultTheme,
+  FlattenInterpolation,
+  ThemeProps,
 } from 'styled-components';
 import { TooltipType } from './Tooltip/Tooltip.data';
 
 interface StyledButtonProps {
-	$buttonTheme?: TooltipType;
+  $buttonTheme?: TooltipType;
 }
 
 const successStyles = css`
-	background-color: green;
-	color: white;
+  background-color: green;
+  color: white;
 `;
 
 const defaultStyles = css`
-	background-color: black;
-	color: white;
+  background-color: black;
+  color: white;
 `;
 
 const errorStyles = css`
-	background-color: red;
-	color: white;
+  background-color: red;
+  color: white;
 `;
 
 const themes: Record<
-	TooltipType,
-	FlattenInterpolation<ThemeProps<DefaultTheme>>
+  TooltipType,
+  FlattenInterpolation<ThemeProps<DefaultTheme>>
 > = {
-	[TooltipType.Default]: defaultStyles,
-	[TooltipType.Success]: successStyles,
-	[TooltipType.Error]: errorStyles,
+  [TooltipType.Default]: defaultStyles,
+  [TooltipType.Success]: successStyles,
+  [TooltipType.Error]: errorStyles,
 };
 
 export const StyledButton = styled.button<StyledButtonProps>`
-	font-size: 2rem;
-	padding: 0.2rem 2.5rem;
-	min-height: 4rem;
-	border-radius: 4.6rem;
-	margin: 0 0.5rem;
-	${({ $buttonTheme }) => $buttonTheme && themes[$buttonTheme]};
+  font-size: 2rem;
+  padding: 0.2rem 2.5rem;
+  min-height: 4rem;
+  border-radius: 4.6rem;
+  margin: 0 0.5rem;
+  ${({ $buttonTheme }) => $buttonTheme && themes[$buttonTheme]};
 `;
